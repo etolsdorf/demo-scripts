a601bcc4c1f81667b836eab9f9a8b689ca4d359f
admin <admin@admin>
2024-10-08 14:52:37 +0530

    minor chnages

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+        title="AI-MD",
+        description="AI-MD",
+        docs_url="/",
+        swagger_ui_oauth2_redirect_url="/docs/oauth2-redirect",
+        openapi_url="/openapi.json",)
 
 @app.get("/")
 def read_root():
